0d7a46567479a7bcd325bb8916152827b91c3b78
sahni007 <varun.sahni04@gmail.com>
2019-08-11 21:56:33 +0530

    update

--- express.js
+++ express.js
@@ -7,26 +7,13 @@ app.get('/auto',(request,response)=>{
 	
 	console.log("Api Hitted...");
 	response.send({
-		"glossary": {
-			"title": "example glossary",
-			"GlossDiv": {
-				"title": "S",
-				"GlossList": {
-					"GlossEntry": {
-						"ID": "SGML",
-						"SortAs": "SGML",
+		
+						"ID": "Smart Grid Detector",
+						"SortAs": "SGDL",
 						"GlossTerm": "Standard Generalized Markup Language",
-						"Acronym": "SGML",
+						"Acronym": "SGD-UCM",
 						"Abbrev": "ISO 8879:1986",
-						"GlossDef": {
-							"para": "A meta-markup language, used to create markup languages such as DocBook.",
-							"GlossSeeAlso": ["GML", "XML"]
-						},
-						"GlossSee": "markup"
-					}
-				}
-			}
-		}
+						
 	});
 	
 })
--- hello_server/hello_server.ino
+++ hello_server/hello_server.ino
@@ -1,87 +1,103 @@
-/* --- node.js script ------------
-const express = require('express');
-const http =require('http');
-
-const app = express();
-
-app.get('/auto',(request,response)=>{
-  
-  console.log("Api Hitted...");
-  response.send("Called");
-  
-})
-
-app.listen(3000, ()=>{
-  console.log("Hello WORLD....");
-});
-
-*/
-
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
-const char* ssid = "Sarver786";
-const char* password = "Devil@007";
- 
-void setup () {
- 
-Serial.begin(9600);
-WiFi.begin(ssid, password);
- 
-while (WiFi.status() != WL_CONNECTED) {
- 
-delay(1000);
-Serial.print("Connecting..");
-//   StaticJsonBuffer<200> jsonBuffer;
-
-  // StaticJsonBuffer allocates memory on the stack, it can be
-  // replaced by DynamicJsonBuffer which allocates in the heap.
-  //
-  // DynamicJsonBuffer  jsonBuffer(200);
-
-  // JSON input string.
-  //
-  // It's better to use a char[] as shown here.
-  // If you use a const char* or a String, ArduinoJson will
-  // have to make a copy of the input in the JsonBuffer.
-//  char json[] =
-//      "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-
-  // Root of the object tree.
-  //
-  // It's a reference to the JsonObject, the actual bytes are inside the
-  // JsonBuffer with all the other nodes of the object tree.
-  // Memory is freed when jsonBuffer goes out of scope.
-//  JsonObject& root = jsonBuffer.parseObject(json);
-//
-//  // Test if parsing succeeds.
-//  if (!root.success()) {
-//    Serial.println("parseObject() failed");
-//    return;
-    }
- 
-}
- 
-void loop() {
- 
-if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
- 
-HTTPClient http;  //Declare an object of class HTTPClient
- 
-http.begin("http://192.168.0.102:3000/auto");  //Specify request destination
-int httpCode = http.GET();                                                                  //Send the request
-
-if (httpCode > 0) { //Check the returning code
-
-String payload = http.getString();   //Get the request response payload
-Serial.println(payload);                     //Print the response payload
- Serial.println("\n\n");
-}
- 
-http.end();   //Close connection
- 
-}
- 
-delay(30000);    //Send a request every 30 seconds
- 
-}
+     
+      /*This is working code of APi hitting to get response in JSON format and parsing the JSON data */
+      
+      /* --- node.js script ------------
+      const express = require('express');
+      const http =require('http');
+      
+      const app = express();
+      
+      app.get('/auto',(request,response)=>{
+        
+        console.log("Api Hitted...");
+        response.send("Called");
+        
+      })
+      
+      app.listen(3000, ()=>{
+        console.log("Hello WORLD....");
+      });
+      
+      */
+      
+      #include <ESP8266WiFi.h>
+      #include <ESP8266HTTPClient.h>
+      #include <ArduinoJson.h>
+//      const char* ssid = "home";
+//      const char* password = "12345678";
+      const char* ssid = "Sarver786";
+      const char* password = "Devil@007";
+      void setup () {
+       
+      Serial.begin(9600);
+      WiFi.begin(ssid, password);
+       
+      while (WiFi.status() != WL_CONNECTED) {
+       
+      delay(1000);
+      Serial.print("Connecting..");
+          
+      }
+      
+      }
+       
+      void loop() {
+       
+      if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+       
+      HTTPClient http;  //Declare an object of class HTTPClient
+       StaticJsonDocument<200> doc;
+      http.begin("http://localhost:3000/auto");  //Specify request destination
+      int httpCode = http.GET();                                                                  //Send the request
+      
+      if (httpCode > 0) { //Check the returning code
+        String payload = http.getString();   //Get the request response payload
+       DeserializationError error = deserializeJson(doc, payload);
+      
+     // Serial.println(payload);                     //Print the response payload
+      // Serial.println("\n\n");
+        const char* ID = doc["ID"];
+            const char* SortAs = doc["SortAs"];
+            const char* GlossTerm = doc["GlossTerm"];
+            const char* Acronym = doc["Acronym"];
+            const char* Abbrev = doc["Abbrev"];
+          
+         // Print values.
+        Serial.println(ID);
+        Serial.println(SortAs);
+      Serial.println(GlossTerm);
+      Serial.println(Acronym);
+      Serial.println(Abbrev);
+      }
+      else{
+        Serial.print("ERROR_CODE");
+        Serial.println(httpCode);
+      }
+       
+      /*
+      {
+        "ID": "SGML",
+        "SortAs": "SGML",
+        "GlossTerm": "Standard Generalized Markup Language",
+        "Acronym": "SGML",
+        "Abbrev": "ISO 8879:1986"
+      }
+      */
+//        char json[] =
+//                    "{\"ID\": \"SGML\",\"SortAs\": \"SGML\",\"Gloss\": \"Standard\",\"Acronym\": \"SGML\",\"Abbrev\": \"ISO\"}";
+//       DeserializationError error = deserializeJson(doc, payload);
+//      
+//      //  // Test if parsing succeeds.
+//        if (error) {
+//             Serial.print(F("deserializeJson() failed: "));
+//          Serial.println(error.c_str());
+//          return;
+//            }
+           
+      http.end();   //Close connection
+       
+      }
+       
+      delay(3000);    //Send a request every 30 seconds
+       
+      }
